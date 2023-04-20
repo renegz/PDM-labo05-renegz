0bcbf4985f76b49349cda18b452f806d6c803ed8
renegz <00077321@uca.edu.sv>
2023-04-19 22:53:49 -0600

    :sparkles: laboratorio 05

---
+++ app/src/main/java/com/example/laboratorio05guzman/BillboardFragment.kt
@@ -0,0 +1,46 @@
+package com.example.laboratorio05guzman
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.navigation.findNavController
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+
+class BillboardFragment : Fragment() {
+
+    private lateinit var actionNavToCreateMovie: FloatingActionButton
+    private lateinit var cardViewMovie: CardView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_billboard, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bind(view)
+        addListerners()
+    }
+
+    private fun bind(view: View){
+        actionNavToCreateMovie = view.findViewById(R.id.action_to_create_movie)
+        cardViewMovie = view.findViewById(R.id.card_view_movie)
+    }
+
+    private fun addListerners(){
+        actionNavToCreateMovie.setOnClickListener{
+            it.findNavController().navigate(R.id.action_billboardFragment_to_createMovieFragment)
+        }
+        cardViewMovie.setOnClickListener{
+            it.findNavController().navigate(R.id.action_billboardFragment_to_movieFragment)
+        }
+    }
+
+}
